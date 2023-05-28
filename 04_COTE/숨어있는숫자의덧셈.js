1f3b59de14b2b35e00c573c85a139acb2805701f
JIN SOL BAIK <121254493+KARIS-SOL@users.noreply.github.com>
2023-05-28 16:06:17 +0900

    숨어있는 숫자의 덧셈

---
+++ 04_COTE/숨어있는숫자의덧셈.js
@@ -0,0 +1,13 @@
+// 문자열 my_string이 매개변수로 주어집니다.
+// my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+
+// 입출력 예 #1
+// "aAb1B2cC34oOp"안의 한자리 자연수는 1, 2, 3, 4 입니다. 따라서 1 + 2 + 3 + 4 = 10 을 return합니다.
+
+// 입출력 예 #2
+// "1a2b3c4d123Z"안의 한자리 자연수는 1, 2, 3, 4, 1, 2, 3 입니다. 따라서 1 + 2 + 3 + 4 + 1 + 2 + 3 = 16 을 return합니다.
+
+function solution(my_string) {
+  var answer = 0;
+  return answer;
+}
